7055a252b2669d7ab1a18ffaa19276f80b910d5d
Odizinne <flora.aubry@proton.me>
2024-09-28 22:00:08 +0200

    fusion on w10

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include "gmm.h"
+#include "utils.h"
 #include <QApplication>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     a.setQuitOnLastWindowClosed(false);
+    if (Utils::isWindows10()) {
+        a.setStyle("fusion");
+    }
     GMM w;
     return a.exec();
 }
